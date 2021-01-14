e6111464e6fce3cd7de9ff828cdd66697ebefdd5
kelly <castrofigueroakelly@gmail.com>
2021-01-13 19:30:22 -0500

    template-interface

--- project/trash/urls.py
+++ project/trash/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.welcome, name='index'),
+    path('about_us/', views.about_us),
+
 ]
--- project/trash/views.py
+++ project/trash/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from rest_framework.renderers import TemplateHTMLRenderer
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def welcome(request):
+    return render(request, 'template.html')
+
+def about_us(request):
+    return render(request, 'test.html')
+
+class UploadImage(APIView):
+    def post(self, request, format=None):
+        #Agregar a la tabla imagen, usar el modelo 
+        print("")
